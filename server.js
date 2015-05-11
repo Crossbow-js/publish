d916635d03b8cedf4cc8b85cb66c1a6b01778240
Shane Osbourne <shane.osbourne8@gmail.com>
2015-05-11 07:25:12 +0100

    Return server handler from module export

--- server.js
+++ server.js
@@ -3,48 +3,7 @@ var http       = require('http');
 var util       = require('util');
 var incoming   = require('./lib/incoming');
 
-http.createServer(server).listen(8080);
-
-function handleUpload (req, res) {
-    return function (err, fields, files) {
-        /**
-         * Ensure the request had a file upload called `release`
-         */
-        if (files.release) {
-
-            /**
-             * Process the incoming file upload
-             */
-            incoming({
-                files: files,
-                fields: fields,
-                flags: JSON.parse(fields.flags)
-            })
-            /**
-             * Handle a successful response
-             */
-            .then(function (output) {
-                /**
-                 * Send a JSON response
-                 */
-                res.writeHead(200, {'content-type': 'application/json'});
-                /**
-                 * Set the status to 'success' and also pass
-                 * along any output (this is typically info about the upload)
-                 */
-                res.end(JSON.stringify({status: 'success', fields: fields, output: output}));
-            })
-            /**
-             * Catch any errors that occured in the file upload
-             * These could be server errors, auth errors, release errors etc
-             */
-            .catch(function (err) {
-                res.writeHead(200, {'content-type': 'application/json'});
-                res.end(JSON.stringify({status: 'error', msg: err.msg}));
-            })
-        }
-    }
-}
+module.exports = server;
 
 /**
  * Simple HTTP server accepting file uploads
@@ -66,4 +25,54 @@ function server (req, res) {
      */
     res.writeHead(401, {'content-type': 'text/plain'});
     res.end('Cannot access this');
+}
+
+/**
+ * Server handler
+ * @param req
+ * @param res
+ * @returns {Function}
+ */
+function handleUpload (req, res) {
+
+    return function (err, fields, files) {
+        /**
+         * Ensure the request had a file upload called `release`
+         */
+        if (files.release) {
+
+            /**
+             * Process the incoming file upload
+             */
+            incoming({
+                files: files,
+                fields: fields,
+                flags: JSON.parse(fields.flags)
+            })
+            /**
+             * Handle a successful response
+             */
+            .then(function (output) {
+                res.writeHead(200, {'content-type': 'application/json'});
+                /**
+                 * Set the status to 'success' and also pass
+                 * along any output (this is typically info about the upload)
+                 */
+                res.end(JSON.stringify({status: 'success', fields: fields, output: output}));
+            })
+            /**
+             * Catch any errors that occured in the file upload
+             * These could be server errors, auth errors, release errors etc
+             */
+            .catch(function (err) {
+                res.writeHead(200, {'content-type': 'application/json'});
+                /**
+                 * Set the status to 'error' and also pass a message
+                 * This should be enough information for the cli
+                 * to give good feedback
+                 */
+                res.end(JSON.stringify({status: 'error', msg: err.msg}));
+            });
+        }
+    };
 }

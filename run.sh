3e1d9d6acfc6dcf0679656a99edc6cf9bd0b8926
Shane Osbourne <shane.osbourne8@gmail.com>
2015-05-13 22:05:25 +0100

    use folder outside of app for sites

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker run -p 80:80 -v $(pwd)/sites:/var/www/sites --name proxy -d nginx-server
-docker run -p 8080:8080 -e CBDEST=/var/www/sites -v $(pwd)/sites:/var/www/sites -d node-release
+docker run -p 80:80 -v /root/sites:/var/www/sites --name proxy -d nginx-server
+docker run -p 8080:8080 -e CBDEST=/var/www/sites -v /root/sites:/var/www/sites -d node-release

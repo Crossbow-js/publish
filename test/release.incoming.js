86d94062e79004bb56f398fb6c4028566f9f9345
Shane Osbourne <shane.osbourne8@gmail.com>
2015-05-15 08:08:47 +0100

    wip refactor

--- test/release.incoming.js
+++ test/release.incoming.js
@@ -21,7 +21,7 @@ describe('Creating a release', function () {
     beforeEach(function () {
         rimraf.sync('releases');
     });
-    it('can create correct symlinks for a release', function (done) {
+    it.only('can create correct symlinks for a release', function (done) {
         var app = http.createServer(server).listen();
         cmd({
             cwd: fixtureDir,
@@ -32,6 +32,7 @@ describe('Creating a release', function () {
         })
         .then(function (resp) {
             var paths = getPaths(resp);
+                console.log(paths);
             assert.isTrue(fs.existsSync(paths.symlink));
             assert.isTrue(fs.existsSync(paths.release));
             assert.isTrue(fs.existsSync(paths.target));

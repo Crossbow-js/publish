8d4975baacd9f28027fd09877c912e4c20ff3972
Shane Osbourne <shane.osbourne8@gmail.com>
2015-05-15 20:42:00 +0100

    cleaup release script

--- rebuild.sh
+++ rebuild.sh
@@ -3,5 +3,5 @@ docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker build -t node-release .
 docker build -t nginx-server docker-images/proxy
-docker run -p 80:80 -v /root/sites:/var/www/sites --name proxy -d nginx-server
-docker run -p 8080:8080 -e CBDEST=/var/www/sites -v /root/sites:/var/www/sites -d node-release
+docker run -p 80:80     --name proxy   -v /root/sites:/var/www/sites -d nginx-server
+docker run -p 8080:8080 --name release -v /root/sites:/var/www/sites -e CBDEST=/var/www/sites -d node-release

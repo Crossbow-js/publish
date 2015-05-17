2ed89130ade3f73e4c1a4b3f3f56b6686019109f
Shane Osbourne <shane.osbourne8@gmail.com>
2015-05-17 16:39:23 +0100

    Restructure project

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker build -t node-release .
-docker build -t nginx-server docker-images/proxy
+docker build -t node-release release
+docker build -t nginx-server proxy
 docker run --name release -v /root/sites:/var/www/sites -e CBDEST=/var/www/sites -d node-release
 docker run --name proxy   -p 80:80 --link release:node-release -v /root/sites:/var/www/sites -d nginx-server

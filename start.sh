02306498ff5bb77864812c45f43378daa57e929d
Shane Osbourne <shane.osbourne8@gmail.com>
2015-05-17 21:09:56 +0100

    Make the dir

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-docker run --name mongo   -d mongo
+mkdir -p /data/db
+docker run --name mongo   -v /data:/data -d mongo
 docker run --name app     -v /root/sites:/var/www/sites --link mongo:mongo -e CBDEST=/var/www/app   -d node-app
 docker run --name release -v /root/sites:/var/www/sites --link mongo:mongo -e CBDEST=/var/www/sites -d node-release
 docker run --name proxy   -p 80:80 --link release:node-release --link app:node-app -v /root/sites:/var/www/sites -d nginx-server

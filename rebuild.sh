9e19120cfeb138fbf818912989f0d92253e9652e
Shane Osbourne <shane.osbourne8@gmail.com>
2015-05-17 16:19:27 +0100

    Remove the need for node-release ports to be exposed

--- rebuild.sh
+++ rebuild.sh
@@ -3,5 +3,5 @@ docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker build -t node-release .
 docker build -t nginx-server docker-images/proxy
-docker run -p 8080:8080 --name release -v /root/sites:/var/www/sites -e CBDEST=/var/www/sites -d node-release
-docker run -p 80:80     -it --name proxy  --link release:node-release -v /root/sites:/var/www/sites -d nginx-server
+docker run --name release -v /root/sites:/var/www/sites -e CBDEST=/var/www/sites -d node-release
+docker run --name proxy   -p 80:80 --link release:node-release -v /root/sites:/var/www/sites -d nginx-server

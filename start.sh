1101f31125c7e371b573650f9664e3ff876c83df
Shane Osbourne <shane.osbourne8@gmail.com>
2015-05-18 07:33:53 +0100

    move to crossbowsites.com

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir -p /Users/shakyshane/crossbow/publish/data/db
-docker run --name db-data -v /Users/shakyshane/crossbow/publish/data:/data busybox
-docker run --name mongo   --volumes-from db-data -d mongo
-docker run --name app     -v /root/sites:/var/www/sites --link mongo:mongo -e CBDEST=/var/www/app   -d node-app
-docker run --name release -v /root/sites:/var/www/sites --link mongo:mongo -e CBDEST=/var/www/sites -d node-release
+#docker run --name db-data -v /Users/shakyshane/crossbow/publish/data:/data busybox
+docker run --name mongo   -d mongo
+docker run --name app     -v /root/sites:/var/www/sites -v $(pwd)/app/node_modules:/src/node_modules --link mongo:mongo -e CBDEST=/var/www/app -d node-app
+docker run --name release -v /root/sites:/var/www/sites -v $(pwd)/release/node_modules:/src/node_modules --link mongo:mongo -e CBDEST=/var/www/sites -d node-release
 docker run --name proxy   -p 80:80 --link release:node-release --link app:node-app -v /root/sites:/var/www/sites -d nginx-server
